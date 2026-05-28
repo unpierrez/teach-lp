7707cb7551a36100b06633c3da4e529668b60f71
Marcos Umpierre <mumpierre98@gmail.com>
2026-05-28 15:54:41 -0300

    Enhance scroll functionality and adjust padding in WhatEveryoneSays component; update global max-width

--- src/app/components/sections/what-everyone-says/useWhatEveryoneSays.hook.js
+++ src/app/components/sections/what-everyone-says/useWhatEveryoneSays.hook.js
@@ -1,32 +1,56 @@
 export function useWhatEveryoneSays() {
-    
+
     const cardsData = [
         {
-        quote: "Lacus vestibulum ultricies mi risus, duis non, volutpat nullam non. Magna congue nisi maecenas elit aliquet eu sed consectetur. Vitae quis cras vitae praesent morbi adipiscing purus consectetur mi.",
-        name: "Hellen Jummy",
-        role: "Financial Counselor",
-        imageSrc: "/assets/users/hellen-jummy.png",
+            quote: "Lacus vestibulum ultricies mi risus, duis non, volutpat nullam non. Magna congue nisi maecenas elit aliquet eu sed consectetur. Vitae quis cras vitae praesent morbi adipiscing purus consectetur mi.",
+            name: "Hellen Jummy",
+            role: "Financial Counselor",
+            imageSrc: "/assets/users/hellen-jummy.png",
         },
         {
-        quote: "Odio rhoncus ornare ut quam. Molestie vel duis quis scelerisque ut id. In tortor turpis viverra sagittis ultrices nisi, nec tortor. Vestibulum, ultrices ultricies neque, hac ultricies dolor.",
-        name: "Ralph Edwards",
-        role: "Math Teacher",
-        imageSrc: "/assets/users/ralph-edwards.png",
+            quote: "Odio rhoncus ornare ut quam. Molestie vel duis quis scelerisque ut id. In tortor turpis viverra sagittis ultrices nisi, nec tortor. Vestibulum, ultrices ultricies neque, hac ultricies dolor.",
+            name: "Ralph Edwards",
+            role: "Math Teacher",
+            imageSrc: "/assets/users/ralph-edwards.png",
         },
         {
-        quote: "Sagittis nunc egestas leo et malesuada urna risus. Morbi proin et cras aliquam. Diam tellus, amet, hac imperdiet. Tellus mi volutpat tellus, congue malesuada sit nisl donec a.",
-        name: "Hellena John",
-        role: "Psychology Student",
-        imageSrc: "/assets/users/hellena-john.png",
+            quote: "Sagittis nunc egestas leo et malesuada urna risus. Morbi proin et cras aliquam. Diam tellus, amet, hac imperdiet. Tellus mi volutpat tellus, congue malesuada sit nisl donec a.",
+            name: "Hellena John",
+            role: "Psychology Student",
+            imageSrc: "/assets/users/hellena-john.png",
         },
         {
-        quote: "Sagittis nunc egestas leo et malesuada urna risus. Morbi proin et cras aliquam. Diam tellus, amet, hac imperdiet. Tellus mi volutpat tellus, congue malesuada sit nisl donec a.",
-        name: "Hellena John",
-        role: "Psychology Student",
-        imageSrc: "/assets/users/hellena-john.png",
+            quote: "Vitae quis cras vitae praesent morbi adipiscing purus consectetur mi. Lacus vestibulum ultricies mi risus, duis non, volutpat nullam non. Magna congue nisi maecenas elit aliquet eu sed.",
+            name: "Hellen Jummy",
+            role: "Financial Counselor",
+            imageSrc: "/assets/users/hellen-jummy.png",
+        },
+        {
+            quote: "In tortor turpis viverra sagittis ultrices nisi, nec tortor. Vestibulum, ultrices ultricies neque, hac ultricies dolor. Odio rhoncus ornare ut quam. Molestie vel duis quis scelerisque.",
+            name: "Ralph Edwards",
+            role: "Math Teacher",
+            imageSrc: "/assets/users/ralph-edwards.png",
+        },
+        {
+            quote: "Diam tellus, amet, hac imperdiet. Tellus mi volutpat tellus, congue malesuada sit nisl donec a. Sagittis nunc egestas leo et malesuada urna risus. Morbi proin et cras aliquam.",
+            name: "Hellena John",
+            role: "Psychology Student",
+            imageSrc: "/assets/users/hellena-john.png",
+        },
+        {
+            quote: "Magna congue nisi maecenas elit aliquet eu sed consectetur. Vitae quis cras vitae praesent morbi adipiscing purus. Lacus vestibulum ultricies mi risus, duis non, volutpat nullam non.",
+            name: "Hellen Jummy",
+            role: "Financial Counselor",
+            imageSrc: "/assets/users/hellen-jummy.png",
+        },
+        {
+            quote: "Vestibulum, ultrices ultricies neque, hac ultricies dolor. Odio rhoncus ornare ut quam. Molestie vel duis quis scelerisque ut id. In tortor turpis viverra sagittis ultrices nisi.",
+            name: "Ralph Edwards",
+            role: "Math Teacher",
+            imageSrc: "/assets/users/ralph-edwards.png",
         },
     ];
-  
+
     return {
         cardsData,
     }
